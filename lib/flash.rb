0df498c3536ac20d8da4b7e4c733c5b57c8a78f7
Jonathan Ahn <jonathanahn95@gmail.com>
2018-09-25 20:39:39 -0400

    Finish flash class

--- lib/flash.rb
+++ lib/flash.rb
@@ -1,4 +1,26 @@
 require 'json'
 
 class Flash
+  attr_reader :now
+
+  def initialize(req)
+    cookie = req.cookies['_fume_app_flash']
+
+    @now = cookie ? JSON.parse(cookie) : {}
+    @flash = {}
+  end
+
+  def [](key)
+    @flash[key.to_s] || @now[key.to_s]
+  end
+
+  def []=(key, value)
+    @flash[key.to_s] = value
+  end
+
+  def store_flash(res)
+    cookie = { path: '/', value: @flash.to_json }
+    res.set_cookie('_fume_app_flash', cookie)
+  end
+
 end
