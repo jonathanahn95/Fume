80747b50c9038b08609c71d5100c61388bbb14be
Jonathan Ahn <jonathanahn95@gmail.com>
2018-09-25 20:35:57 -0400

    Work on Router class

--- lib/router.rb
+++ lib/router.rb
@@ -24,20 +24,39 @@ class Router
   attr_reader :routes
 
   def initialize
+    @routes = []
+
   end
 
   def add_route(pattern, method, controller_class, action_name)
+    @routes << Route.new(pattern, method, controller_class, action_name)
   end
 
+
   def draw(&proc)
+    self.instance_eval(&proc)
+
   end
 
   [:get, :post, :put, :delete].each do |http_method|
+    define_method(http_method) do |pattern, controller_class, action_name|
+      add_route(pattern,http_method,controller_class, action_name)
+    end
   end
 
   def match(req)
+    @routes.each do |route|
+      return route if route.matches?(req)
+    end
+    nil
   end
 
   def run(req, res)
+    if match(req)
+      match(req).run(req, res)
+    else
+      res.status = 404
+    end
+
   end
 end
