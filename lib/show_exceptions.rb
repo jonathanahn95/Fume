889c089480e386f4c23458518b5a4bb84b45cf94
Jonathan Ahn <jonathanahn95@gmail.com>
2018-09-25 20:41:28 -0400

    Add ShowExceptions class

--- lib/show_exceptions.rb
+++ lib/show_exceptions.rb
@@ -1,15 +1,29 @@
 require 'erb'
+require 'byebug'
 
 class ShowExceptions
+  attr_reader :app
+
   def initialize(app)
+    @app = app
   end
 
   def call(env)
+    begin
+      app.call(env)
+    rescue Exception => e
+      render_exception(e)
+    end
   end
 
   private
 
+
   def render_exception(e)
+
+    ['500', {'Content-type' => 'text/html'}, ["RuntimeError"]]
   end
+
+
 
 end
