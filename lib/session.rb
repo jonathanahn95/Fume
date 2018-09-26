228b7587fd6235c50175aab83e9eaf9ce7968edb
Jonathan Ahn <jonathanahn95@gmail.com>
2018-09-25 20:33:53 -0400

    Work on session

--- lib/session.rb
+++ lib/session.rb
@@ -1,19 +1,25 @@
 require 'json'
-
+require 'byebug'
 class Session
-  # find the cookie for this app
-  # deserialize the cookie into a hash
   def initialize(req)
+    cookie = req.cookies["_fume_app"]
+    if cookie
+      @info = JSON.parse(cookie)
+    else
+      @info = {}
+    end
   end
 
   def [](key)
+    @info[key]
   end
 
   def []=(key, val)
+    @info[key] = val
   end
 
-  # serialize the hash into json and save in a cookie
-  # add to the responses cookies
   def store_session(res)
+    cookie = { path: '/', value: @info.to_json }
+    res.set_cookie("_rails_lite_app", cookie)
   end
 end
